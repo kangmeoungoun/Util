3283e9c75e7aad80edfb8a50df45069baf7e209f
kmo <kes98201@gmail.com>
2020-09-09 20:54:47 +0900

    added output "tell her World"

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello{
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("Tll your World");
-		Ststem.out.println("Tell her World");
+		System.out.println("Tell her World");
 	}
 }
 
